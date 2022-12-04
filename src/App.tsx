d5352927d9b8ef502f00a0c49361b012310a60cc
almedinhodzic <almedin.json@gmail.com>
2022-12-04 13:11:42 +0100

    Added react refresh

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./styles.css";
 import image from "./react.png";
 import logo from "./logo-react.svg";
+import { ClickCounter } from "./ClickCounter";
 
 export const App = () => {
   return (
@@ -8,6 +9,7 @@ export const App = () => {
       <h1>React Typescript Webpack Template - {process.env.NODE_ENV} - {process.env.name}</h1>
       <img src={image} alt="React Logo" width={400} height={400} />
       <img src={logo} alt="React Logo" width={400} />
+      <ClickCounter />
     </>
   );
 }
