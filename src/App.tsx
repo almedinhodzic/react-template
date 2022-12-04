800785aef935386807fafd1c165a282a35487d19
almedinhodzic <almedin.json@gmail.com>
2022-12-04 11:06:38 +0100

    Configured rules for images and svgs

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import "./styles.css";
+import image from "./react.png";
+import logo from "./logo-react.svg";
+
 export const App = () => {
-  return <h1>React Typescript Webpack Template</h1>
+  return (
+    <>
+      <h1>React Typescript Webpack Template</h1>
+      <img src={image} alt="React Logo" width={400} height={400} />
+      <img src={logo} alt="React Logo" width={400} />
+    </>
+  );
 }
